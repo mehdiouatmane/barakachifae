534bc316dcdba1ddc5629a7c681c0a561cdb1b2c
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2023-04-16 18:44:05 +0000

    Add files via upload

--- addtocart.php
+++ addtocart.php
@@ -142,7 +142,7 @@ fbq('track', 'PageView');
 		<div class="col2a">  <div class="col2b"> 			 
 		 </div></div>			   
 		<div class="col3a">    <div class="col3b">
-				<a href="index.php">  <img src="img/siro/barakachifae1.jpg" class="logo"  />	</a>    
+				<a href="index.php">  <img src="img/product/siro/barakachifae1.jpg" class="logo"  />	</a>    
 		</div></div> 		  
 		<div class="col4a">    <div class="col4b">
 				  <a style="display:none" class="search">   <i class="fas fa-search ">  </i> </a> 
