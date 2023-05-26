fca2e6cc83d1ee713b8142b8e637daab61a958d2
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2023-05-26 15:20:27 +0100

    Add files via upload

--- legal/about-us.php
+++ legal/about-us.php
@@ -157,7 +157,7 @@ html, body{ background-color:#ecf0f1;  padding:0; border:0; margin:0% 0%;  touch
 					<div> <a href="contact-us.php">Contact us</a> </div>
 				</div>				
 			    <div class="footer">
-				   <div class="text"> CopyRight © 2022 - barakachifae </div>
+				   <div class="text"> CopyRight © 2023 - Barakachifae </div>
 				</div>
 			</div></div>			
 	</div>
